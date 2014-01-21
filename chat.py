2ed394fc04b83ea20e0ccb9f7aafc44cf2964582
funkybob <curtis@tinbrain.net>
2014-01-21 18:41:52 +1100

    Initial commit

---
+++ chat.py
@@ -0,0 +1,257 @@
+import json
+import random
+import re
+import string
+import os.path
+from functools import partial
+import mimetypes
+
+
+import bleach
+import redis
+from sse import Sse
+
+from urlparse import parse_qs
+from Cookie import SimpleCookie
+
+pool = redis.ConnectionPool()
+
+# Rate limiting ?
+
+RANDOM_CHARS = string.letters + string.digits
+def random_string(source=RANDOM_CHARS, length=32):
+    return ''.join([
+        random.choice(source)
+        for x in xrange(length)
+    ])
+
+def get_template(name):
+    fn = os.path.join('templates/', name)
+    with file(fn, 'rb') as fin:
+        return fin.read()
+
+##
+
+def make_key(*args):
+    return u':'.join(args)
+
+##
+## Nick handling
+##
+
+def get_nicks(request):
+    keys = request.conn.keys(make_key(request.channel, '*', 'nick'))
+    if not keys:
+        return {}
+    return {
+        key: value
+        for key, value in zip(keys, request.conn.mget(keys))
+    }
+
+def get_nick(request):
+    key = make_key(request.channel, request.tag, 'nick')
+    n = request.conn.get(key)
+    if n is None:
+        n = request.tag[:8]
+        set_nick(request, n)
+    else:
+        request.conn.expire(key, 90)
+    return n
+
+def set_nick(request, name):
+    name = strip_tags(name)
+    names = get_nicks(request).keys()
+    if name in names:
+        raise ValueError('Nick in use!')
+    key = make_key(request.channel, request.tag, 'nick')
+    request.conn.set(key, name, ex=90)
+    return name
+
+##
+## Message handling
+##
+
+def post_message(request, message, mode='message', queue=None, **data):
+    if queue is None:
+        queue = make_key(request.channel, 'channel')
+
+    data.setdefault('message', message)
+    data.setdefault('sender', get_nick(request))
+
+    content = json.dumps(data)
+    request.conn.publish(queue, json.dumps([mode, content]))
+
+strip_tags = partial(bleach.clean, tags=[], strip=True)
+
+def linkify_external(attrs, new=False):
+    attrs['target'] = '_blank'
+    return attrs
+
+def clean_message(msg):
+    '''Clean up and process messages before sending'''
+    return bleach.linkify(strip_tags(msg), callbacks=[linkify_external,])
+
+##
+## The application!
+##
+
+class Request(object):
+    def __init__(self, environ):
+        self.environ = environ
+
+        cookies = environ.get('HTTP_COOKIE', '')
+        if cookies == '':
+            cookies = {}
+        else:
+            c = SimpleCookie()
+            c.load(cookies)
+            cookies = {
+                key: c.get(key).value
+                for key in c.keys()
+            }
+        self.cookies = cookies
+        self.QUERY_DATA = parse_qs(environ.get('QUERY_STRING', ''))
+        self.method = environ['REQUEST_METHOD']
+
+class Response(object):
+    def __init__(self, content='', status=200, content_type='text/html'):
+        self.content = content
+        self.status = status
+        self.headers = {}
+        self.headers['Content-Type'] = content_type
+        self.cookies = SimpleCookie()
+
+    def add_cookie(self, key, value, **kwargs):
+        self.cookies[key] = value
+
+class Response404(Response):
+    def __init__(self, content=''):
+        super(Response404, self).__init__(content, 404)
+
+STATUS = {
+    200: '200 OK',
+    404: '404 Not Found',
+}
+
+def application(environ, start_response):
+
+    request = Request(environ)
+
+    chatterbox_tag = request.cookies.get('chatterbox')
+    if not chatterbox_tag:
+        request.tag = random_string()
+    else:
+        request.tag = chatterbox_tag
+
+    path = environ.get('PATH_INFO', '/')
+
+    # Dispatch
+    response = Response404()
+    for pattern in urlpatterns:
+        m = re.match(pattern[0], path)
+        if m:
+            response = pattern[1](request, **m.groupdict())
+
+    if not chatterbox_tag:
+        response.cookies['chatterbox'] = request.tag
+
+    headers = list(response.headers.items()) + [
+        ('Set-Cookie', cookie.OutputString())
+        for cookie in response.cookies.values()
+    ]
+
+    start_response(STATUS[response.status], response.headers.items())
+    return response.content
+
+def index(request):
+    return Response(get_template('index.html'))
+
+def chat(request, channel=None):
+    request.channel = channel
+
+    request.conn = redis.StrictRedis(connection_pool=pool)
+
+    if request.method == 'GET':
+        if not 'text/event-stream' in request.environ['HTTP_ACCEPT']:
+            return Response(get_template('chat.html'))
+
+        pubsub = request.conn.pubsub()
+        pubsub.subscribe([])
+
+        def _iterator():
+            sse = Sse()
+            for msg in pubsub.listen():
+                if msg['type]'] == 'message':
+                    mode, data = json.loads(msg['data'])
+                    sse.add_message(mode, data)
+                    for item in sse:
+                        yield item
+
+        post_message(request, '{} connected.'.format(get_nick(request)), 'join', sender='Notice')
+
+        response = Response(_iterator(), content_type='text/event-stream')
+        response.headers['Cache-Control'] = 'no-cache'
+
+    elif request.method == 'POST':
+
+        nick = get_nick(request)
+
+        mode = request.QUERY_DATA.get('mode', 'message')
+        msg = request.QUERY_DATA.get('msg', '')
+
+        if mode == 'nick' and msg:
+            try:
+                new_nick = set_nick(request, msg)
+            except ValueError:
+                post_message(request, 'Nick in use!', 'alert', sender='Notice')
+            else:
+                post_message(request, '{} is now known as {}'.format(nick, new_nick),
+                    mode='nick',
+                    sender='Notice'
+                )
+
+        elif mode == 'names':
+            post_message(request, get_nicks(request).keys(), 'names')
+
+        elif mode == 'msg':
+            target = request.QUERY_DATA['target']
+            nicks = self.get_nicks(request)
+            nick_map = { v: k for k, v in nicks.items() }
+            msg = clean_message(msg)
+            _, target_tag, _ = nick_map[target].split(':')
+            post_message(request, msg, 'msg', target=target,
+                queue=make_key(target_tag, 'private')
+            )
+            post_message(request, 'msg', target=target,
+                queue=make_key(request.tag, 'private')
+            )
+
+        elif mode in ['message', 'action']:
+            post_message(request, clean_message(msg), mode)
+
+        else:
+            log.warning('Unknown message: %r', mode)
+
+        response = Response()
+
+    else:
+        response = Response('', status=405)
+
+    return response
+
+def static(request, filename):
+    try:
+        fin = open(os.path.join('static/', filename), 'rb')
+        content_type, encoding = mimetypes.guess_type(filename)
+        content_type = content_type or 'application/octet-stream'
+
+        return Response(fin.read(), content_type=content_type)
+    except:
+        return Response404()
+
+urlpatterns = [
+    (r'^/$', index, ),
+    (r'^/static/(?P<filename>.*)$', static,),
+    (r'^/(?P<channel>.+)/$', chat, ),
+]
+
---
+++ static/js/chatterbox.js
@@ -0,0 +1,207 @@
+// Standard django CSRF handling
+function getCookie(name) {
+    var i, cookie, cookies, cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        cookies = document.cookie.split(';');
+        for (i = 0; i < cookies.length; i++) {
+            cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+// Simple template renderer
+function Render(tmpl, data) {
+    return tmpl.replace(/\{(\w+)\}/g, function (match, key) { return data[key]; });
+};
+
+// EventSource malarky
+var ChatterBox = (function () {
+    var modemap = {}, input, messages, nicks, source, url;
+
+    var template = {
+        message : '<div class="message {mode}"><time>{when}</time><span>{sender}</span><p>{message}</p></div>',
+        action  : '<div class="message action"><time>{when}</time><p><i>{sender}</i> {message}</p></div>',
+        join    : '<div class="message join"><time>{when}</time><p><i>{message}</i></p></div>',
+        nick    : '<div class="message nick"><time>{when}</time><p><i>{message}</i></p></div>',
+        msg     : '<div class="message msg"><time>{when}</time><span><i>{sender}</i> &rArr; <i>{target}</i></span><p><em>{message}</em></p></div>'
+    };
+
+    // Encode an obj to POST format
+    function postEncode(obj) {
+        var result = [];
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key)) {
+                result.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+            }
+        }
+        return result.join('&');
+    };
+
+    // Send a message to server
+    function send(message, mode, extra) {
+        var xhr = new window.XMLHttpRequest();
+
+        extra = extra || {};
+        extra.message = message;
+        extra.mode = mode;
+
+        xhr.open('POST', url);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        xhr.send(postEncode(extra));
+    };
+
+    // Print message to screen
+    function append_message(data, tmpl) {
+        data.mode = tmpl;
+        tmpl = template[tmpl] || template['message'];
+        data.when = data.when || moment().format('H:mm:ss');
+        messages.innerHTML += Render(tmpl, data);
+        messages.scrollTop = 9999999;
+        Array.prototype.slice.call(
+            messages.querySelectorAll('.message'), 0, -1000
+        ).map(function (el) { messages.removeChild(el); });
+    };
+
+    // Parse event data and render message
+    function parse_message(event, tmpl) {
+        var data = JSON.parse(event.data);
+        append_message(data, tmpl || 'message');
+    }
+
+    function setStatus(state) {
+        input.classList.remove(input.classList[0]);
+        input.classList.add(state);
+    };
+
+    modemap['open'] = function (event) {
+        setStatus('ready');
+        send('', 'ping');
+    };
+
+    modemap['error'] = function (event) {
+        if(event.readyState == EventSource.CLOSED) {
+            setStatus('disconnected');
+            connect();
+        } else {
+            setStatus('error');
+        }
+    };
+
+    modemap['action'] = function (event) { parse_message(event, 'action'); };
+
+    modemap['message'] = function (event) { parse_message(event, 'message'); };
+
+    modemap['note'] = function (event) { parse_message(event, 'note'); };
+
+    modemap['join'] = function (event) { send('', 'names'); parse_message(event, 'join'); };
+
+    modemap['nick'] = function (event) { send('', 'names'); parse_message(event, 'nick'); };
+
+    modemap['msg'] = function (event) { parse_message(event, 'msg'); };
+
+    modemap['names'] = function (event) {
+        var data = JSON.parse(event.data);
+        var content = [];
+        for(var i=0, j=data.message.length; i < j ; i++ ) {
+            content.push('<li>' + data.message[i] + '</li>');
+        }
+        nicks.innerHTML = content.join('\n');
+    };
+
+    function connect () {
+        setStatus('connecting');
+        source = new EventSource(url);
+        for(var key in modemap) {
+            if(modemap.hasOwnProperty(key)) {
+                source.addEventListener(key, modemap[key], false);
+            }
+        };
+    };
+
+    function keypress(e) {
+        var extra = {};
+        if( e.keyCode == 9 ) {
+            // Nick complete
+            // parse back for the last space to now.
+            var match = /(\w+)$/i.exec(input.value);
+            if(match) {
+                var pfx = match[1], pattern = RegExp('^' + match[1], 'i');
+                // Now find if it matches a known nick
+                var nl = nicks.querySelectorAll('li');
+                for(var i=0; i < nl.length; i++) {
+                    if(pattern.test(nl[i].innerHTML)) {
+                        input.value = input.value.slice(0, -pfx.length) + nl[i].innerHTML;
+                        input.value += (input.value.length == nl[i].innerHTML.length) ? ': ' : ' ';
+                        break;
+                    }
+                }
+            }
+            e.preventDefault();
+            return false;
+        }
+        if( e.keyCode != 13 ) return;
+        var msg = input.value;
+        if(msg.length == 0) return;
+        var mode = 'message'
+        var match = /^\/(\w+)\s?(.+)/g.exec(msg);
+        if(match) {
+            switch(match[1]) {
+            case 'nick':
+                var match = /^(\w+)/.exec(match[2]);
+                mode = 'nick';
+                msg = match[1];
+                break;
+            case 'me':
+                mode = 'action';
+                msg = match[2];
+                break;
+            case 'names':
+                mode = 'names';
+                msg = '';
+                break;
+            case 'msg':
+                var match = /([-\w]+)\s+(.+)/.exec(match[2]);
+                mode = 'msg';
+                extra.target = match[1];
+                msg = match[2];
+                break;
+            default:
+                break;
+            }
+        }
+        send(msg, mode, extra);
+        clear();
+    };
+
+    function clear () {
+        input.value = '';
+        input.focus();
+    };
+
+    function init (root_url) {
+        url = root_url;
+        input = document.querySelector('#input input');
+        messages = document.querySelector('#messages');
+        nicks = document.querySelector('#nicks ul');
+        // Attach input handling, and connect
+        input.addEventListener('keydown', keypress);
+        clear();
+        connect();
+        window.setInterval(ChatterBox.send, 30000, '', 'names');
+    };
+
+    return {
+        init: init,
+        send: send,
+    };
+})();
+
+document.addEventListener('DOMContentLoaded', function () { ChatterBox.init('.'); }, false);

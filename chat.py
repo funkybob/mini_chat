2c5791bcacd1e35f3b31f524bf2142709f2a6619
Curtis <curtis@tinbrain.net>
2015-01-24 17:22:52 +1100

    Try to conver to asyncio

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
+import asyncio
 from functools import partial
 from http.cookies import SimpleCookie
 import json
@@ -9,16 +10,15 @@ import string
 import time
 from urllib.parse import parse_qs
 
+import asyncio_redis
 import bleach
-import redis
 
 import logging
+#logging.basicConfig(level=logging.DEBUG)
 
-ROOT = os.path.dirname(__file__)
-POOL = redis.ConnectionPool()
 
 RATE_LIMIT_DURATION = 60
-RATE_LIMIT = 10
+RATE_LIMIT = 100
 
 STATUS_OK = '200 OK'
 STATUS_NOT_FOUND = '404 Not Found'
@@ -34,31 +34,32 @@ make_key = lambda *args: ':'.join(args)
 
 # Nick handling
 def get_nicks(request):
-    keys = request.conn.keys(make_key(request.channel, '*', 'nick'))
-    return {
-        k.decode('utf-8'): v.decode('utf-8')
-        for k, v in zip(request.conn.mget(keys), keys)
-    } if keys else {}
+    keys = yield from request.conn.keys(make_key(request.channel, '*', 'nick'))
+    if keys:
+        values = yield from request.conn.mget(keys)
+        yield dict(zip(values, keys))
+    yield {}
 
 
 def get_nick(request):
     key = make_key(request.channel, request.tag, 'nick')
-    nick = request.conn.get(key)
+    nick = yield from request.conn.get(key)
     if nick is None:
-        nick = set_nick(request, request.tag[:8])
+        nick = yield from set_nick(request, request.tag[:8])
     else:
-        nick = nick.decode('utf-8')
-        request.conn.expire(key, 90)
-    return nick
+        nick = nick
+        _ = yield from request.conn.expire(key, 90)
+    yield nick
 
 
 def set_nick(request, name):
     name = strip_tags(name)
-    if name in get_nicks(request):
+    nicks = yield from get_nicks(request)
+    if name in nicks:
         raise ValueError('Nick in use!')
     key = make_key(request.channel, request.tag, 'nick')
-    request.conn.set(key, name, ex=90)
-    return name
+    _ = yield from request.conn.set(key, name, ex=90)
+    yield name
 
 
 # Message handling
@@ -70,7 +71,7 @@ def post_message(request, message, mode='message', queue=None, **data):
     data.setdefault('sender', get_nick(request))
 
     content = json.dumps(data)
-    request.conn.publish(queue, json.dumps([mode, content]))
+    next(request.conn.publish(queue, json.dumps([mode, content])))
 
 strip_tags = partial(bleach.clean, tags=[], strip=True)
 
@@ -82,7 +83,7 @@ def linkify_external(attrs, new=False):
 
 # The application!
 class Request(object):
-    def __init__(self, environ):
+    def __init__(self, environ, conn):
         self.environ = environ
 
         self.method = environ['REQUEST_METHOD']
@@ -91,44 +92,40 @@ class Request(object):
         self.cookies = self.parse_cookies()
         self.query_data = self.parse_query_data()
 
-        self.conn = redis.StrictRedis(connection_pool=POOL)
+        self.conn = conn
 
     def parse_cookies(self):
         cookie_data = self.environ.get('HTTP_COOKIE', '')
-        if cookie_data == '':
-            return {}
-        else:
-            cookies = SimpleCookie()
+        cookies = SimpleCookie()
+        if cookie_data:
             cookies.load(cookie_data)
-            return {key: cookies.get(key).value for key in cookies.keys()}
+        return {key: cookies.get(key).value for key in cookies.keys()}
 
     def parse_query_data(self):
-        if self.method == 'GET':
-            src = parse_qs(self.environ.get('QUERY_STRING', ''))
-        elif self.method == 'POST':
-            # Should test content type
+        if self.method == 'POST':
             size = int(self.environ.get('CONTENT_LENGTH', 0))
             if not size:
                 return {}
-            src = parse_qs(self.environ['wsgi.input'].read(size))
+            src = self.environ['wsgi.input'].read(size)
         else:
-            return {}
+            src = self.environ.get('QUERY_STRING', '')
         return {
             k.decode('utf-8'): [x.decode('utf-8') for x in v]
-            for k, v in src.items()
+            for k, v in parse_qs(src).items()
         }
 
 
 class Response(object):
     def __init__(self, content=None, status=STATUS_OK, content_type=None):
-        self.content = content or ''
+        self.content = '' if content is None else content
         self.status = status
         self.headers = {'Content-Type': content_type or 'text/html'}
         self.cookies = SimpleCookie()
 
 
 def application(environ, start_response):
-    request = Request(environ)
+    conn = yield from asyncio_redis.Pool.create(host='localhost', port=6379, poolsize=10)
+    request = Request(environ, conn)
     # Session cookie
     tag = request.cookies.get('chatterbox')
     if not tag:
@@ -139,11 +136,12 @@ def application(environ, start_response):
         request.tag = tag
     # Rate limiting
     key = make_key(request.tag, 'rated')
+
     now = int(time.time())
-    pipe = request.conn.pipeline(transaction=False)
-    pipe.zadd(key, now, now).expireat(key, now + RATE_LIMIT_DURATION)
-    pipe.zremrangebyscore(key, '-inf', now - RATE_LIMIT_DURATION)
-    size = pipe.zcard(key).execute()[-1]
+    next(conn.zadd(key, {str(now): float(now)}))
+    next(conn.expireat(key, now + RATE_LIMIT_DURATION))
+    next(conn.zremrangebyscore(key, asyncio_redis.ZScoreBoundary.MAX_VALUE, asyncio_redis.ZScoreBoundary(float(now - RATE_LIMIT_DURATION))))
+    size = yield from conn.zcard(key)
     if size > RATE_LIMIT:
         response = Response(status=STATUS_RATE_LIMITED)
     else:
@@ -178,14 +176,20 @@ def chat(request, channel=None):
         if 'text/event-stream' not in request.environ['HTTP_ACCEPT']:
             return Response(get_template('chat.html'))
 
-        pubsub = request.conn.pubsub()
-        pubsub.subscribe([
-            make_key(request.channel, 'channel'),
-            make_key(request.tag, 'private'),
-        ])
-
         def _iterator():
-            for msg in pubsub.listen():
+            nick = yield from get_nick(request)
+            _ = yield from post_message(request, '{} connected.'.format(nick), 'join',
+                         sender='Notice')
+
+            pubsub = yield from request.conn.start_subscribe()
+            _ = yield from pubsub.subscribe([
+                make_key(request.channel, 'channel'),
+                make_key(request.tag, 'private'),
+            ])
+
+            while True:
+                msg = yield from pubsub.next_published()
+                print(msg)
                 if msg['type'] == 'message':
                     mode, data = json.loads(msg['data'].decode('utf-8'))
                     yield 'event: {}\n'.format(mode).encode('utf-8')
@@ -193,50 +197,51 @@ def chat(request, channel=None):
                         yield 'data: {}\n'.format(line).encode('utf-8')
                     yield '\n'.encode('utf-8')
 
-        post_message(request, '{} connected.'.format(get_nick(request)),
-                     'join', sender='Notice')
-
         response = Response(_iterator(), content_type='text/event-stream')
         response.headers['Cache-Control'] = 'no-cache'
 
     elif request.method == 'POST':
 
-        nick = get_nick(request)
+        def _generator():
+            nick = yield from get_nick(request) # Triggers timeout update
 
-        mode = request.query_data.get('mode', ['message'])[0]
-        msg = request.query_data.get('message', [''])[0]
-        msg = bleach.linkify(strip_tags(msg), callbacks=[linkify_external])
+            mode = request.query_data.get('mode', ['message'])[0]
+            msg = request.query_data.get('message', [''])[0]
+            msg = bleach.linkify(strip_tags(msg), callbacks=[linkify_external])
 
-        if mode == 'nick' and msg:
-            try:
-                new_nick = set_nick(request, msg)
-            except ValueError:
-                post_message(request, 'Nick in use!', 'alert', sender='Notice')
+            if mode == 'nick' and msg:
+                try:
+                    new_nick = yield from set_nick(request, msg)
+                except ValueError:
+                    _ = yield from post_message(request, 'Nick in use!', 'alert', sender='Notice')
+                else:
+                    _ = yield from post_message(request,
+                                 '{} is now known as {}'.format(nick, new_nick),
+                                 mode='nick',
+                                 sender='Notice')
+
+            elif mode == 'names':
+                nicks = yield from get_nicks(request)
+                _ = yield from post_message(request, list(nicks.keys()), 'names')
+
+            elif mode == 'msg':
+                target = request.query_data['target'][0]
+                nicks = yield from get_nicks(request)
+                _, target_tag, _ = nicks[target].split(':')
+                _ = yield from post_message(request, msg, 'msg', target=target,
+                             queue=make_key(target_tag, 'private'))
+                _ = yield from post_message(request, msg, 'msg', target=target,
+                             queue=make_key(request.tag, 'private'))
+
+            elif mode in ['message', 'action']:
+                _ = yield from post_message(request, msg, mode)
+
             else:
-                post_message(request,
-                             '{} is now known as {}'.format(nick, new_nick),
-                             mode='nick',
-                             sender='Notice')
+                logging.warning('Unknown message: %r', mode)
 
-        elif mode == 'names':
-            post_message(request, list(get_nicks(request).keys()), 'names')
+            yield b''
 
-        elif mode == 'msg':
-            target = request.query_data['target'][0]
-            nicks = get_nicks(request)
-            _, target_tag, _ = nicks[target].split(':')
-            post_message(request, msg, 'msg', target=target,
-                         queue=make_key(target_tag, 'private'))
-            post_message(request, msg, 'msg', target=target,
-                         queue=make_key(request.tag, 'private'))
-
-        elif mode in ['message', 'action']:
-            post_message(request, msg, mode)
-
-        else:
-            logging.warning('Unknown message: %r', mode)
-
-        response = Response()
+        response = Response(_generator())
 
     else:
         response = Response(status=STATUS_METHOD_NOT_ALLOWED)
@@ -245,13 +250,18 @@ def chat(request, channel=None):
 
 
 def static(request, filename):
+    root = os.path.dirname(__file__)
     try:
-        fin = open(os.path.join(ROOT, 'static/', filename), 'rb')
-        content_type, encoding = mimetypes.guess_type(filename)
-        content_type = content_type or 'application/octet-stream'
-        return Response(fin, content_type=content_type)
+        fin = open(os.path.join(root, 'static/', filename), 'rb')
     except:
         return Response(status=STATUS_NOT_FOUND)
+    else:
+        content_type, encoding = mimetypes.guess_type(filename)
+        content_type = content_type or 'application/octet-stream'
+        def _reader(f):
+            yield f.read()
+            f.close()
+        return Response(_reader(fin), content_type=content_type)
 
 URLPATTERNS = [
     (r'^/$', index, ),
